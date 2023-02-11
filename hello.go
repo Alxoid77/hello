69541b6e3b5eed32b3fcbd1f0f385667f91697b4
Alxoid77 <alxoid77@gmail.com>
2023-02-11 10:53:42 +0200

    sample telega bot

--- hello.go
+++ hello.go
@@ -1,23 +1,34 @@
 package main
 
 import (
-	"fmt"
 	"log"
-	"net/http"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
 func main() {
-	http.HandleFunc("/", handler) // Каждый запрос вызывает обработчик
-	log.Fatal(http.ListenAndServe("localhost:8000", nil))
-}
+	bot, err := tgbotapi.NewBotAPI("MyAwesomeBotToken")
+	if err != nil {
+		log.Panic(err)
+	}
 
-// Обработчик возвращает компонент пути из URL запроса,
-func handler(w http.ResponseWriter, r *http.Request) {
-	//var resp string
-	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
-	//w = formatter(resp)
-}
+	bot.Debug = true
 
-func formatter(input string) string {
-	return input
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message != nil { // If we got a message
+			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+			msg.ReplyToMessageID = update.Message.MessageID
+
+			bot.Send(msg)
+		}
+	}
 }
