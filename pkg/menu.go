725eb45393a25c9a29079edd86327dc2660d7971
Alxoid77 <alxoid77@gmail.com>
2023-02-11 15:11:11 +0200

    /

--- pkg/menu.go
+++ pkg/menu.go
@@ -14,22 +14,3 @@ func GetMainMenuKeyboard() tgbotapi.ReplyKeyboardMarkup {
 		),
 	)
 }
-
-/*
-func GetElectricityMenuKeyboard(chatId int64) tgbotapi.ReplyKeyboardMarkup {
-	var subscribtionElectricityCommand = SUBSCRIBE_ELECTRICITY_COMMAND
-
-	if IsElectricitySubscribed(chatId) {
-		subscribtionElectricityCommand = UNSUBSCRIBE_ELECTRICITY_COMMAND
-	}
-
-	return tgbotapi.NewReplyKeyboard(
-		tgbotapi.NewKeyboardButtonRow(
-			tgbotapi.NewKeyboardButton(subscribtionElectricityCommand),
-		),
-		tgbotapi.NewKeyboardButtonRow(
-			tgbotapi.NewKeyboardButton(MAIN_MENU_COMMAND),
-		),
-	)
-}
-*/
